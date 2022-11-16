d82033e825033962720b5a70624342a7c7daeca7
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-16 19:12:10 +0530

    ADDED: Code that records logs at application runtime environment

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,22 @@
 package org.example;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static final Logger LOG= LogManager.getLogger();
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        String message="hi";
+        LOG.info("info");
+        LOG.error("Error");
+        LOG.debug(message+"Debug");
+        LOG.fatal("fatal");
     }
 }
